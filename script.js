1fb6a6b2d1e8680317092d7227327959e3a7fcc5
Жека <fadeev12evgenii65@gmail.com>
2026-05-12 17:33:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-body {
-	background-color: #2f3542;
-	color: white;
-	font-family; 'Segoe UI', Tahoma, Genevam, Verdana, sans-serif;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	height: 100vh;
-	margin: 0;
-}
+const btn = document.getElementByTd('magicButton');
+const title = document.getElementById('mainTitle');
+
+btn.addEventListener('click', () => {
+
+	title.innerText = "Ура! Ты запустил ядерку!";
+	title.style.color = "#39ff14";
+
+	alert("Поздравляю! Ты уничтожил всю планету! У тебя есть 5 минут для того чтобы спрятаться в бункер :)");
+
+	document.body.style.backgroundColor = "#57606f";
+});
